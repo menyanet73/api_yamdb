4c2d806df822749d16f6968eb41ff8da04bb7c0b
Maxim <menyanet73@gmail.com>
2022-02-19 18:13:55 +0300

    changed csv script, not full complete

--- api_yamdb/reviews/management/commands/importfile2.py
+++ api_yamdb/reviews/management/commands/importfile2.py
@@ -1,43 +1,57 @@
-import csv, codecs, sqlite3
+import csv, codecs
 
 from django.core.management.base import BaseCommand
-
+from reviews.models import Category, Title, Review, User, Genre, Comment
 
 class Command(BaseCommand):
-    help = 'Import csv file, genre_title files import will be after title and genre files'
+    help = 'Import csv file'
 
     def add_arguments(self, parser):
         parser.add_argument('file')
 
     def handle(self, *args, **options):
         file = options['file']
+        file_name = file.partition('.')[0]
         path = f'static/data/{file}'
-        table_name = file.partition('.')[0]
-        con = sqlite3.connect('db.sqlite3')
-        cur = con.cursor()
         with codecs.open(path, encoding='utf-8') as f:
             reader = csv.reader(f)
-            firstrow = next(reader)
-            reader.__next__()
-            for raw in reader:
-                if table_name in table_names:
-                    table_name = table_names[table_name]
-                # if table_name == 'genre_title':
-                #     table_name = 'title_genre'
-                # if table_name == 'users':
-                #     table_name = 'user'
-                if file == 'titles.csv':
-                    # table_name = 'title'
-                    if 'description' not in firstrow:
-                        raw.insert(3, '')
-                print(raw)
-                print(f'INSERT INTO reviews_{table_name} VALUES {tuple(raw)}')
-                cur.execute(f'INSERT INTO reviews_{table_name} VALUES {tuple(raw)};')
-        con.commit()
-        con.close()
+            headers = next(reader)
 
-table_names = {
-    'genre_title': 'title_genre',
-    'users': 'user',
-    'titles': 'title',
+            ''' Определение имен для орм команды '''
+            model = model_names[f'{file_name}']
+            method = 'create'
+            if file_name == 'genre_title':
+                method = 'update'
+
+            ''' Определение полей указываемых в модели'''
+            print(headers)
+            for row in reader:
+                fields = ''
+                string_orm = ''
+                for header, field in zip(headers,row):
+                    if 'id'  in header or header in int_fields:
+                        if header == 'category' or header == 'genre':
+                            header += '_id'
+                            print(header)
+                    elif header == 'text':
+                        field = f'"{field}"' # Нужно придумать как решить проблему с переносом строки в этом поле
+                    else:
+                        field = f'"{field}"'
+                    fields += f'{header}={field}, '
+                fields = fields[:-2] # Удалим последнюю запятую
+                string_orm = f'{model}.objects.{method}({fields})'
+                print(string_orm)
+                exec(string_orm)
+                # Category.objects.create(name=row[1], slug=row[2])
+
+model_names = {
+    'category': 'Category',
+    'comments': 'Comment',
+    'genre_title': 'Title',
+    'genre': 'Genre',
+    'review': 'Review',
+    'titles': 'Title',
+    'users': 'User',
 }
+
+int_fields = ['year', 'pub_date', 'category', 'genre', 'author', 'score']
