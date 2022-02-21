94eb6bf7a75800a42e5fa51b33f1fea232e27abc
Maxim <menyanet73@gmail.com>
2022-02-21 13:25:05 +0300

    permission mini fix

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -38,7 +38,11 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
         return request.user.role in ['admin', 'superuser']
 
     def has_object_permission(self, request, view, obj):
+        if not request.user.is_authenticated:
+            return False
         return request.user.role in ['admin', 'superuser']
