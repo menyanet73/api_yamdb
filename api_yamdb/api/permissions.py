d13b5848b824e4ffab0cea3cdc927be054d6071a
Konstantin Kintushev <k.kintushev@gmail.com>
2022-02-22 22:23:42 +0300

    pep-8

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,20 +1,6 @@
 from rest_framework import permissions
 
 
-#  TODO Не уверен что этот пермишн нужен, возможно следует удалить
-# class IsUserOrAdmin(permissions.BasePermission):
-#     message = "У Вас не достаточно прав, обратитесь к администратору."
-
-#     def has_permission(self, request, view):
-#         return True
-
-#     def has_object_permission(self, request, view, obj):
-#         if (request.method in ['GET', 'PATCH']
-#                 and obj.role == request.user.role):
-#             return True
-#         return obj.role in ['admin', 'superuser']
-
-
 class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
     message = "У Вас не достаточно прав, обратитесь к администратору."
 
@@ -33,13 +19,14 @@ class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated 
-            and request.user.role in ['admin', 'superuser'])
+                or request.user.is_authenticated
+                and request.user.role in ['admin', 'superuser'])
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_superuser
                 or request.user.role in ['admin', 'superuser'])
+
 
 class IsAdmin(permissions.BasePermission):
     message = "У Вас не достаточно прав, обратитесь к администратору."
