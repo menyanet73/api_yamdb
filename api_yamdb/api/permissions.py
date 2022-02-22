4589c2a3166993857da7925355315b96f1a55db4
Konstantin Kintushev <k.kintushev@gmail.com>
2022-02-22 22:20:12 +0300

    Added superuser to all permissions)

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -26,6 +26,7 @@ class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.role in ['admin', 'superuser', 'moderator']
                 or obj.author == request.user
+                or request.user.is_superuser
                 )
 
 
@@ -37,8 +38,8 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-                and request.user.role in ['admin', 'superuser'])
+                or request.user.is_superuser
+                or request.user.role in ['admin', 'superuser'])
 
 class IsAdmin(permissions.BasePermission):
     message = "У Вас не достаточно прав, обратитесь к администратору."
@@ -46,10 +47,12 @@ class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
-        return request.user.role in ['admin', 'superuser']
+        return (request.user.role in ['admin', 'superuser']
+                or request.user.is_superuser)
 
     def has_object_permission(self, request, view, obj):
         if not request.user.is_authenticated:
             return False
-        return (request.user.role in ['admin', 'superuser']
+        return (request.user.is_superuser
+                or request.user.role in ['admin', 'superuser']
                 or request.user == obj)
